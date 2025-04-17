da12bd2f65a6103f21df8578093220d3344eddf0
Bartłomiej Wołk <bartlomiejwolk@gmail.com>
2025-04-17 12:28:53 +0200

    Change "Aider" to "Aider_msns"

--- aider/coders/base_coder.py
+++ aider/coders/base_coder.py
@@ -195,7 +195,7 @@ class Coder:
 
     def get_announcements(self):
         lines = []
-        lines.append(f"Aider v{__version__}")
+        lines.append(f"Aider_msns v{__version__}")
 
         # Model
         main_model = self.main_model
